f94303c83fad5d6073bae44fb06a08ad9afc19cf
nazar <naixcsgo@gmail.com>
2022-04-02 23:03:38 +0300

    added new logic parsing site

--- bot_main.py
+++ bot_main.py
@@ -18,32 +18,25 @@ class Parser:
         return link
 
     def save_to_file_links(self):
-        file = open('text.txt', 'w')
-        for index in self.find_links():
-            file.write(index + '\n')
-        file.close()
+        with open('text.txt', 'w') as file:
+            for index in self.find_links():
+                file.write(index + '\n')
 
     def checking_product_availability(self):
-        with open('text.txt', 'a+') as opened_file:
-            for l in self.find_links():
-                if l != opened_file:
-                    logger.warning("YES new LINK")
-                    opened_file.write(l)
+        links = self.find_links()
+        with open('text.txt', 'r+') as opened_file:
+            a = opened_file.read()
+            for line in links:
+                if line in a:
+                    logger.info("Не найденно новых товаров")
                 else:
-                    logger.info('now new links')
+                    opened_file.write(line)
+                    logger.warning("Найден новый товар, был заменен в спсике.")
 
-
-# end_list = open('end_list.txt','w')
-# listdir = open('listdir.txt')
-# order_set = set(open('order_fix.txt').readlines())
-
-# for line in listdir.readlines():
-#    if line in order_set:
-#        end_list.write(line)
 
 bot = Parser(
     "https://www.olx.ua/elektronika/telefony-i-aksesuary/q-iphone-13/?search%5Bfilter_float_price%3Afrom%5D=10000&search%5Border%5D=created_at%3Adesc",
-    "//a[@data-cy = 'listing-ad-title']/@href")
+    '//td[@class = "offer  "]/div[@class = "offer-wrapper"]/table/tbody/tr/td[2]/div/h3/a/@href')
 
 logger.info("Parsing of all links from the site.")
 bot.find_links()
