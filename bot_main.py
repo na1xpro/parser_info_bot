bce36c19c4fc5d983c71ea3c6934025695ed11c3
nazar <naixcsgo@gmail.com>
2022-04-05 21:06:51 +0300

    Added check for outdated product data.

--- bot_main.py
+++ bot_main.py
@@ -1,52 +1,47 @@
 from lxml import html
-import requests
 from loguru import logger
-
-url = (
-    "https://www.olx.ua/elektronika/telefony-i-aksesuary/q-iphone-13/?search%5Bfilter_float_price%3Afrom%5D=10000&search%5Border%5D=created_at%3Adesc")
+import requests
 
 
 class Parser:
-    def __init__(self, url, xpath_link,xpath_price):
+    def __init__(self, url, xpath_link, xpath_price):
         self.link_xpath = xpath_link
         self.url = url
         self.price_xpath = xpath_price
 
-
     def find_links_and_price(self):
-        response = requests.get(url)
+        response = requests.get(self.url)
         parsed_body = html.fromstring(response.text)
         link = parsed_body.xpath(self.link_xpath)
         price = parsed_body.xpath(self.price_xpath)
-        print(price)
-        return link
+        return link, price
 
     def save_to_file_links(self):
+        links, prices = self.find_links_and_price()
         with open('text.txt', 'w') as file:
-            for index in self.find_links_and_price():
-                file.write(index + '\n')
+            for lin, pri in zip(links, prices):
+                file.write(' ID  Товару - |' + lin + "| Цына товару " + pri + "\n")
+        return file
 
     def checking_product_availability(self):
-        links = self.find_links_and_price()
         with open('text.txt', 'r+') as opened_file:
             a = opened_file.read()
-            for line in links:
-                if line in a:
+            links, prices = self.find_links_and_price()
+            for lines, pricess in zip(links, prices):
+                if lines and pricess in a:
                     logger.info("Не найденно новых товаров")
                 else:
-                    opened_file.write(line)
-                    logger.warning("Найден новый товар, был заменен в спсике.")
+                    self.save_to_file_links()
+                    logger.warning("Найденые новые твоары, были заменены в файле.")
 
-# block = page.xpath('//table[@id="offers_table»]')[0]
-# links = block.xpath('.//a[@data-cy="listing-ad-title"]/@href')
+
 bot = Parser(
-    "https://www.olx.ua/uk/list/q-iphone/",'//table[@id = "offers_table"]//td[@class = "offer  "]//table/@data-id',
-    '//table[@id = "offers_table"]//td[@class = "offer  "]//p[@class = "price"]/@text')
-
-
+    "https://www.olx.ua/uk/list/q-iphone/", '//table[@id = "offers_table"]//td[@class = "offer  "]//table/@data-id',
+    '//table[@id = "offers_table"]//td[@class = "offer  "]//p[@class = "price"]/strong/text()')
 logger.info("Parsing of all links from the site.")
 bot.find_links_and_price()
-logger.info("Adding links to a file.")
+logger.add("Adding links and prices to a file.")
 bot.save_to_file_links()
+logger.info("Links and prices have been added.")
 logger.info('Chek file in links')
 bot.checking_product_availability()
