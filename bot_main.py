5172df6ad34e954154d7019397c0f304bf379e09
nazar <naixcsgo@gmail.com>
2022-04-29 23:37:47 +0300

    fixed problems (not all)

--- bot_main.py
+++ bot_main.py
@@ -4,14 +4,15 @@ import requests
 from datetime import datetime
 import time
 from bot_constants import constants
-import sqlite3
 
 
 class Parser:
-    def __init__(self, url, xpath_link, xpath_price, ):
+    def __init__(self, url, xpath_link, xpath_price):
         self.url = url
         self.link_xpath = xpath_link
         self.price_xpath = xpath_price
+        self.name_save_file = 'text.txt'
+        self.links = self.find_links_and_price()
 
     def find_links_and_price(self):
         response = requests.get(self.url)
@@ -20,17 +21,14 @@ class Parser:
         price = parsed_body.xpath(self.price_xpath)
         return index, price
 
-
-
-    def save_to_file_links(self, find_links):
-        links, prices = find_links
-        with open('text.txt', 'w') as file:
-            for lin, pri in zip(links, prices):
+    def save_to_file_links(self):
+        with open(self.name_save_file, 'w') as file:
+            for lin, pri in zip(self.links):
                 file.write(f"ID  Товару - | {lin} | Ціна товару - {pri} \n")
 
-    def chekinfile(self, data):
+    def chek_in_file(self, data):
         links, prices = data
-        with open('text.txt', 'r+') as file:
+        with open(self.name_file, 'r+') as file:
             a = file.read()
             for lines, pricess in zip(links, prices):
                 if lines and pricess in a:
@@ -39,13 +37,14 @@ class Parser:
                     file.write(f"ID  Товару - | {lines} | Ціна товару - {pricess} - НОВИЙ ТОВАР \n")
                     logger.warning("Знайдені нові товари, були замінені у файлі.")
 
+
 bot = Parser(constants['URL'], constants['xpath_link'], constants['xpath_price'])
 while True:
     now = datetime.now()
     logger.info("Парсинг посилань та цін із сайту.")
     bot.find_links_and_price()
     logger.info("Додавання посилань та цін у файл.")
-    bot.save_to_file_links(bot.find_links_and_price())
+    bot.save_to_file_links()
     logger.info("Посилання та ціни були додані до файлу.")
     logger.info('Перевірка на наявність нового товару.')
     bot.chekinfile(bot.find_links_and_price())
