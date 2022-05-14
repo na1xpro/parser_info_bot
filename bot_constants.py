6081b81d68f7f8b275f7ef745540aeeae028ea6d
nazar <naixcsgo@gmail.com>
2022-05-14 19:02:13 +0300

    fix new errors

--- bot_constants.py
+++ bot_constants.py
@@ -1,5 +1,6 @@
 constants = {
     'URL': "https://www.olx.ua/uk/list/q-iphone/",
-    'xpath_link': '//table[@id = "offers_table"]//td[@class = "offer  "]//table/@data-id',
+    'xpath_id': '//table[@id = "offers_table"]//td[@class = "offer  "]//table/@data-id',
     'xpath_price': '//table[@id = "offers_table"]//td[@class = "offer  "]//p[@class = "price"]/strong/text()',
+    'xpath_links_product': '//table[@id = "offers_table"]//td[@class = "offer  "]//a[@class = "thumb vtop inlblk rel tdnone linkWithHash  scale4 detailsLink "]/@href'
 }
--- bot_main.py
+++ bot_main.py
@@ -7,52 +7,70 @@ import sqlite3
 
 
 class Parser:
-    def __init__(self, url, xpath_link, xpath_price, ):
+    def __init__(self, url, id_product_xpath, xpath_price, links_product):
         self.url = url
-        self.link_xpath = xpath_link
+        self.id_product_xpath = id_product_xpath
         self.price_xpath = xpath_price
+        self.links_product = links_product
         self.name_database = 'base.db'
-        self.sql_cursor, self.db = self.connect_database()
-        self.ind, self.pric = self.find_links_and_price()
+        self.conn, self.cursor = self.connect_database()
 
-    def find_links_and_price(self):
+    def parse_data(self):
         response = requests.get(self.url)
         parsed_body = html.fromstring(response.text)
-        index = parsed_body.xpath(self.link_xpath)
-        price = parsed_body.xpath(self.price_xpath)
-        return index, price
+        ids_list = parsed_body.xpath(self.id_product_xpath)
+        prices_list = parsed_body.xpath(self.price_xpath)
+        links_prod = parsed_body.xpath(self.links_product)
+        list_product = []
+        products_id_price = {
+        }
+        products_links = {
+        }
+        links_key = "links"
+        for id_prod, price_prod in zip(ids_list, prices_list):
+            products_id_price[id_prod] = price_prod
+        list_product.append(products_id_price)
+        for prod_links in links_prod:
+            products_links[prod_links] = links_key
+        list_product.append(products_links)
+        return list_product
 
     def connect_database(self):
-        db = sqlite3.connect(self.name_database)
-        sql = db.cursor()
+        conn = sqlite3.connect(self.name_database)
+        cursor = conn.cursor()
         logger.info("Звязок з базою данних встановлено.")
-        return sql, db
+        return conn, cursor
 
-    def initialization_and_filling_the_base(self):
-        self.sql_cursor.execute("""CREATE TABLE IF NOT EXISTS product(
-                indeх TEXT,
+    def initialization_table(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS product(
+                id_product INT,
                 price TEXT,
-                status TEXT
-    
-            )""")
-        self.db.commit()
-        for ind, prices in zip(self.ind, self.pric):
-            self.sql_cursor.execute("INSERT INTO product VALUES (?,?,?)", (ind, prices, "NEW"))
-        self.db.commit()
-        logger.info("Данн доданы")
+                links TEXT
+        )
+        """)
+        self.conn.commit()
+        logger.info(" Таблиця додана ")
 
-    def revision_new_data(self):
-        for ind, prices in zip(self.ind, self.pric):
-            self.sql_cursor.execute("Update product set  indeх = ?,price = ?,status = 'NEWSW'", (ind, prices))
-            self.db.commit()
-        logger.info("Записи оновлений")
+    def filling_table(self, product_data):
+        for (index, prices), links in zip(product_data[0].items(), product_data[1].keys()):
+            self.cursor.execute("INSERT INTO product VALUES (?,?,?)", (index, prices, links))
+            self.conn.commit()
+        logger.info("Даннi були доданi до таблицi")
+
+    # def revision_new_data(self, product):
+    #     for index, prices in product.items():
+    #         self.cursor.execute("Update product set  id_product = ?, price = ?, status = 'NEWSW'", (index, prices))
+    #         self.conn.commit()
+    #     logger.info("Записи оновлені")
+
 
 
 #
 #
-bot = Parser(constants['URL'], constants['xpath_link'], constants['xpath_price'])
+bot = Parser(constants['URL'], constants['xpath_id'], constants['xpath_price'], constants['xpath_links_product'])
 now = datetime.now()
-logger.info("Парсинг посилань та цін із сайту.")
-bot.find_links_and_price()
-# bot.initialization_and_filling_the_base()
-bot.revision_new_data()
+logger.info("Парсинг данних з сайту.")
+product_data = bot.parse_data()
+bot.initialization_table()
+bot.filling_table(product_data)
