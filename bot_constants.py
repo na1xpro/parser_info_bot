416c103d29d676dd51aea007e71072d0a2398424
nazar <naixcsgo@gmail.com>
2022-06-09 19:15:01 +0300

    fixed 09

--- bot_constants.py
+++ bot_constants.py
@@ -1,7 +1,4 @@
+
 constants = {
-    'URL': "https://www.olx.ua/uk/list/q-iphone/",
-    'xpath_name': '//div[@class = "css-3xiokn" and not (div[@data-testid = "adCard-featured"])]/ancestor-or-self::div[@type = "list"][1]/div[@class = "css-13aawz3"]/../div/../../div[@class = "css-9nzgu8"]/div/h6/text()',
-    'xpath_price': '//div[@class = "css-3xiokn" and not (div[@data-testid = "adCard-featured"])]/ancestor-or-self::div[@type = "list"][1]/div[@class = "css-13aawz3"]/../div/../../div[@class = "css-9nzgu8"]/div/p[@class = "css-l0108r-Text eu5v0x0"]/text()',
-    'xpath_links_product': "//div[@class = 'css-3xiokn' and not (div[@data-testid = 'adCard-featured'])]/ancestor::a/@href",
-    # 'xpath_addition data': "//div[@class = 'css-3xiokn' and not (div[@data-testid = 'adCard-featured'])]/ancestor-or-self::div[@type = 'list'][1]/div[@class = 'css-13aawz3']/../div/../../div[@class = 'css-9nzgu8']/div[2]/p[@class = 'css-p6wsjo-Text eu5v0x0']/text()"
+    'URL': "https://www.olx.ua/d/uk/list/q-iphone/",
 }
--- bot_main.py
+++ bot_main.py
@@ -3,38 +3,43 @@ from loguru import logger
 import requests
 from datetime import datetime
 from bot_constants import constants
+from lxml.html import fromstring
 import sqlite3
 
 
 class Parser:
-    def __init__(self, url, name_product_xpath, xpath_price, links_product, ):
+    def __init__(self, url):
         self.url = url
-        self.id_product_xpath = name_product_xpath
-        self.price_xpath = xpath_price
-        self.links_product = links_product
-        # self.addition_data = addition_data #Надо доабвить в инициализацию
         self.name_database = 'base.db'
-        self.url_website = "https://www.olx.ua"
         self.conn, self.cursor = self.connect_database()
 
+    def unpack_dict(self, key_):
+        unpack_list = []
+        for iterator_key in product_data:
+            key = iterator_key.get(key_)
+            unpack_list.append(key)
+        return unpack_list
+
     def parse_data(self):
+        product_list = []
         response = requests.get(self.url)
-        parsed_body = html.fromstring(response.text)
-        name_prod_list = parsed_body.xpath(self.id_product_xpath)
-        prices_list = parsed_body.xpath(self.price_xpath)
-        links_prod = parsed_body.xpath(self.links_product)
-        # addition_data = parsed_body.xpath(self.addition_data)
-        products_data_list = []
-        for id_prod, price_prod, link_prod in zip(name_prod_list, prices_list, links_prod, ):
-            products_dict = {
-                "name_prod": id_prod,
-                "price": price_prod,
-                "link": self.url_website + link_prod,
-                # 'ad_info': add_data
-            }
-            products_data_list.append(products_dict)
-        print(products_data_list)
-        return products_data_list
+        tree = fromstring(response.text)
+        products = tree.xpath('//div[@data-cy="l-card"]')
+        for product in products:
+            top = product.xpath('.//div[@data-testid="adCard-featured"]')
+            if not top:
+                name = product.xpath(".//h6/text()")[0]
+                price = product.xpath('.//div[@type="list"]/div/text()')
+                price = price[0] if price else None
+                link = "https://www.olx.ua" + product.xpath(".//a/@href")[0]
+                for name_prod, price_prod, link_prod in zip([name], [price], [link]):
+                    products_dict = {
+                        "name": name_prod,
+                        "price": price_prod,
+                        "link": link_prod
+                    }
+                    product_list.append(products_dict)
+        return product_list
 
     def connect_database(self):
         conn = sqlite3.connect(self.name_database)
@@ -45,7 +50,7 @@ class Parser:
     def initialization_table(self):
         self.cursor.execute("""
         CREATE TABLE IF NOT EXISTS product(
-                name_product INT,
+                name TEXT,
                 price TEXT,
                 links TEXT
         )
@@ -53,33 +58,33 @@ class Parser:
         self.conn.commit()
         logger.info(" Таблиця додана ")
 
-    def filling_table(self, product_data):
-        def unpack_dict(key_):
-            unpack_list = []
-            for iterator_key in product_data:
-                key = iterator_key.get(key_)
-                unpack_list.append(key)
-            return unpack_list
-
-        for name, price, link in zip(unpack_dict('name_prod'), unpack_dict('price'), unpack_dict('link')):
+    def filling_table(self):
+        for name, price, link in zip(self.unpack_dict('name'), self.unpack_dict('price'), self.unpack_dict('link')):
             self.cursor.execute("INSERT INTO product VALUES (?,?,?)", (name, price, link))
             self.conn.commit()
 
     logger.info("Даннi були доданi до таблицi")
 
-
-# def revision_new_data(self, product):
-#     for index, prices in product.items():
-#         self.cursor.execute("Update product set  id_product = ?, price = ?, status = 'NEWSW'", (index, prices))
-#         self.conn.commit()
-#     logger.info("Записи оновлені")
+    def revision_new_data(self):
+        for name_prod in self.unpack_dict('name'):
+            self.cursor.execute(f"SELECT name FROM product WHERE name = '{name_prod}'")
+            if self.cursor.fetchone() is None:
+                for name, price, link in zip(self.unpack_dict('name'), self.unpack_dict('price'),
+                                             self.unpack_dict('link')):
+                    self.cursor.execute("""Update product set name = ?,price = ?,links = ?""", (name, price, link))
+                    self.conn.commit()
+                    logger.info('Був додадний новий товар')
+                else:
+                    logger.info('Не було додано  нового товара')
+                    self.conn.commit()
 
 
 #
 #
-bot = Parser(constants['URL'], constants['xpath_name'], constants['xpath_price'], constants['xpath_links_product'])
+bot = Parser(constants['URL'])
 now = datetime.now()
 logger.info("Парсинг данних з сайту.")
 product_data = bot.parse_data()
 bot.initialization_table()
-bot.filling_table(product_data)
+bot.filling_table()
+bot.revision_new_data()
