2ba2d910b2b402eaa7bb6f328e8a98a3252005e2
nazar <naixcsgo@gmail.com>
2022-06-13 22:52:05 +0300

    fixed 10

--- bot_main.py
+++ bot_main.py
@@ -1,10 +1,10 @@
 from lxml import html
 from loguru import logger
 import requests
-from datetime import datetime
 from bot_constants import constants
 from lxml.html import fromstring
 import sqlite3
+import re
 
 
 class Parser:
@@ -13,78 +13,91 @@ class Parser:
         self.name_database = 'base.db'
         self.conn, self.cursor = self.connect_database()
 
-    def unpack_dict(self, key_):
-        unpack_list = []
-        for iterator_key in product_data:
-            key = iterator_key.get(key_)
-            unpack_list.append(key)
-        return unpack_list
-
-    def parse_data(self):
-        product_list = []
+    def get_data(self):
+        """Підклчюення парсера до сайту"""
         response = requests.get(self.url)
         tree = fromstring(response.text)
+        return tree
+
+    def parse_data(self, tree):
+        """Парсинг данних з сайту"""
+        product_list = []
         products = tree.xpath('//div[@data-cy="l-card"]')
         for product in products:
             top = product.xpath('.//div[@data-testid="adCard-featured"]')
             if not top:
                 name = product.xpath(".//h6/text()")[0]
                 price = product.xpath('.//div[@type="list"]/div/text()')
-                price = price[0] if price else None
+                for price_currency in price:
+                    global currency
+                    global new_price
+                    currency = re.sub(r'\d+\s?', '', price_currency).strip()
+                    new_price = re.sub(r'[а-я]+\s?', '', price_currency).strip()
+
                 link = "https://www.olx.ua" + product.xpath(".//a/@href")[0]
-                for name_prod, price_prod, link_prod in zip([name], [price], [link]):
-                    products_dict = {
-                        "name": name_prod,
-                        "price": price_prod,
-                        "link": link_prod
-                    }
-                    product_list.append(products_dict)
+                products_dict = {
+                    "name": name,
+                    "price": new_price,
+                    'currency': currency,
+                    "link": link,
+
+                }
+                product_list.append(products_dict)
+
+        # print(product_list)
         return product_list
 
     def connect_database(self):
+        """Підключення до бази данних"""
         conn = sqlite3.connect(self.name_database)
         cursor = conn.cursor()
         logger.info("Звязок з базою данних встановлено.")
         return conn, cursor
 
     def initialization_table(self):
+        """Створення таблиці"""
         self.cursor.execute("""
-        CREATE TABLE IF NOT EXISTS product(
-                name TEXT,
-                price TEXT,
-                links TEXT
-        )
-        """)
+            CREATE TABLE IF NOT EXISTS product(
+                    name TEXT,
+                    price INT,
+                    links TEXT,
+                    currency TEXT
+            )
+            """)
         self.conn.commit()
-        logger.info(" Таблиця додана ")
 
-    def filling_table(self):
-        for name, price, link in zip(self.unpack_dict('name'), self.unpack_dict('price'), self.unpack_dict('link')):
-            self.cursor.execute("INSERT INTO product VALUES (?,?,?)", (name, price, link))
-            self.conn.commit()
+    def filling_table(self, product_data: list[dict]):
+        """Заповнення  данних в таблицю"""
+        info = self.cursor.execute('SELECT * FROM product')
+        if info.fetchone() is None:
+            logger.info('Таблиця була не заповненою')
+            for products in product_data:
+                self.cursor.execute("INSERT INTO product VALUES (?,?,?,?)",
+                                    (products['name'], products['price'], products['link'], products['currency']))
+                self.conn.commit()
+        else:
+            logger.info('Таблиця вже заповнена')
 
     logger.info("Даннi були доданi до таблицi")
 
-    def revision_new_data(self):
-        for name_prod in self.unpack_dict('name'):
-            self.cursor.execute(f"SELECT name FROM product WHERE name = '{name_prod}'")
-            if self.cursor.fetchone() is None:
-                for name, price, link in zip(self.unpack_dict('name'), self.unpack_dict('price'),
-                                             self.unpack_dict('link')):
-                    self.cursor.execute("""Update product set name = ?,price = ?,links = ?""", (name, price, link))
-                    self.conn.commit()
-                    logger.info('Був додадний новий товар')
-                else:
-                    logger.info('Не було додано  нового товара')
-                    self.conn.commit()
+    def revision_new_data(self, products_data: list[dict]):
+        """Перевірка  на актуальність данних"""
+        for products in products_data:
+            chek_link = self.cursor.execute(f"SELECT links FROM product WHERE links = '{products['link']}'")
+            if chek_link.fetchone() is None:
+                self.cursor.execute("""Update product set name = ?,price = ?,links = ?,currency = ?""", (
+                    products['name'], products['price'], products['link'], products['currency']))
+                self.conn.commit()
+                logger.info('Був додадний новий товар')
+            else:
+                logger.info('Не було додано  нового товара')
 
 
 #
 #
 bot = Parser(constants['URL'])
-now = datetime.now()
 logger.info("Парсинг данних з сайту.")
-product_data = bot.parse_data()
+product_data = bot.parse_data(bot.get_data())
 bot.initialization_table()
-bot.filling_table()
-bot.revision_new_data()
+bot.filling_table(product_data)
+bot.revision_new_data(product_data)
